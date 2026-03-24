1c3ba45adc47e2d330ba2131d361cf58bfc50256
kimgeon <kimgeon0802@gmail.com>
2026-03-24 14:10:34 +0900

    Update File

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     ]);
   const [nextId, setNextId] = useState(3); // 이미 0,1,2 가 존재하므로 3으로 설정
 
-  // 이벤트핸들러 함수 선언
+  // 이벤트핸들러 함수 선언..
   const handleChange = (e) => {
     setTodo(e.target.value); // input field의 다음 바뀔 값
   };
